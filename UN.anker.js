665019d9566774755fdac9c47f9c68f721c6125b
iki1uc <entertrainer@web.de>
2026-07-23 03:48:59 +0200

    Update UN.anker.js

--- UN.anker.js
+++ UN.anker.js
@@ -1,1 +1,19 @@
+class UN_ANCHOR {
+
+    constructor(){
+        this.value = 0;
+        this.stabil = 0;
+    }
+
+    set(v){
+        this.value = v;
+        this.stabil = Math.sqrt(v * v);
+    }
+
+    get(){
+        return this.stabil;
+    }
+}
+
+window.UN_ANCHOR = new UN_ANCHOR();
 
